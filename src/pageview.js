94631a6ad72650dd3562cdc53e6a3cbb7efcf271
Sakari Jokinen <sakariij@gmail.com>
2013-01-19 22:59:32 +0000

    local links work

--- src/pageview.js
+++ src/pageview.js
@@ -1,8 +1,22 @@
 (function() {
+    function relativeTo(from, to) {
+        var dirname = from.replace(/[^\/]+$/, '')
+        return dirname + to
+    }
+
     function pageview(container, url) {
         console.log('loading ' + url);
         $.get(url, function(data) {
-            container.html(marked(data))
+            var document = $(marked(data))
+            document.find('a').each(function(ix, value) {
+                value = $(value)
+                value.attr('href', relativeTo(url, value.attr('href')));
+                value.click(function(event) {
+                    pageview(container, value.attr('href'))
+                    event.preventDefault()
+                })
+            })
+            container.html(document)
         })
     }
 
